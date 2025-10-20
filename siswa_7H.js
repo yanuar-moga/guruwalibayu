29411fde133e00c45d8f0b2b0060ef9287555c23
MB <oppoa5s.mbcell@gmail.com>
2025-10-20 19:27:05 +0700

    Add files via upload

---
+++ siswa_7H.js
@@ -0,0 +1,15 @@
+// /data/siswa_7H.js
+
+const dataKelas7H = {
+    namaKelas: "7H",
+    waliKelas: "Yanuar Bayu Triyono",
+    emailWali: "yanuar.bayu62@guru.smp.belajar.id",
+    logoSekolah: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgYCPiGhuoDL1mp18rPAMUCt98ADIyg7C297Bx4H53sxM12CtZE89tThO6KUe14s5cR0p6z8DrFQCHUJNXa1tXuFN45DJcNgU0uOW7D00l9cdkRDHUpyHFOfcHtg91R01_CXpPKKXEVS4orocyah8Z_gel0EAg6jrXno_84uYR6UDlxzpmHXOhby8Z0vyQ/s3558/LOGO%20SMP%201%20MOGA.png",
+    siswa: [
+        { nama: "FATHAN GUSTOMY", jenisKelamin: "L", kontakOrtu: "" },
+        { nama: "IBTISAM ARIFA", jenisKelamin: "P", kontakOrtu: "" },
+        { nama: "INAYATI AENA", jenisKelamin: "P", kontakOrtu: "" },
+        // Lanjutkan sisa 17 data siswa lainnya di sini...
+        { nama: "NUR WANIFA", jenisKelamin: "P", kontakOrtu: "" }
+    ]
+};
